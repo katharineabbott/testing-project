f912f2df2d8f3243a2a3aa9111c0775e0d9f258a
katharineabbott <katharinemabbott@gmail.com>
2018-09-12 17:31:08 -0700

    create "initial entries" suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -82,6 +82,7 @@ $(function() {
     }); 
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -89,6 +90,10 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+    });
+
+        
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
